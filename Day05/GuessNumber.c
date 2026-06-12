1bc175b6eb2e2e8a2ebbd44dde2669c576f16022
狄佳驹 <dijiaju_2024@qq.com>
2026-06-12 12:39:09 +0800

    Add number guessing game in GuessNumber.c

--- Day05/GuessNumber.c
+++ Day05/GuessNumber.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+    int guess = 0;
+
+    srand((unsigned int)time(NULL));
+
+    int target = rand() % 100 + 1;
+
+    printf("猜数字游戏开始！\n");
+
+    while(1)
+    {
+        printf("请输入1~100之间的数字：");
+        scanf("%d", &guess);
+
+        if(guess > target)
+        {
+            printf("猜大了！\n");
+        }
+        else if(guess < target)
+        {
+            printf("猜小了！\n");
+        }
+        else
+        {
+            printf("恭喜你猜对了！\n");
+            break;
+        }
+    }
+
+    return 0;
+}
