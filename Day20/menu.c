2527dd660882f9df9096d2b62fcc412c87ee8ace
狄佳驹 <dijiaju_2024@qq.com>
2026-07-04 16:27:16 +0800

    Create menu.c

---
+++ Day20/menu.c
@@ -0,0 +1,13 @@
+#include "student.h"
+
+void Menu()
+{
+    printf("\n====================\n");
+    printf("   学生管理系统\n");
+    printf("====================\n");
+    printf("1. 添加学生\n");
+    printf("2. 显示学生\n");
+    printf("3. 查找学生\n");
+    printf("0. 退出\n");
+    printf("====================\n");
+}
